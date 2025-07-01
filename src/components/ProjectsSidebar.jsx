8693030af5cca5f16435736daedc6d73dc2c2fb8
leonardoLoddo <20loddoloddo@gmail.com>
2025-07-01 10:48:38 +0200

    aggiunto componente sidebar e stilizzato attraverso tailwind

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function ProjectsSidebar() {
+  return (
+    <aside className="bg-stone-900 px-8 py-16 rounded-r-xl w-1/3 md:w-72 text-stone-50">
+      <h2 className="mb-8 font-bold text-stone-200 md:text-xl uppercase">
+        I tuoi Progetti
+      </h2>
+      <div>
+        <button className="bg-stone-700 hover:bg-stone-600 px-4 py-2 rounded text-stone-400 hover:text-stone-100 text-xs md:text-base">
+          + Aggiungi progetto
+        </button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
